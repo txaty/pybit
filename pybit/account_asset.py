b82b57c96bdbf6d704d4e3eee5912f32526aa257
kevinwon96 <107445491+kevinwon96@users.noreply.github.com>
2022-07-26 09:20:07 -0400

    Change doc links

--- pybit/account_asset.py
+++ pybit/account_asset.py
@@ -200,8 +200,50 @@ class HTTP(_HTTPManager):
 
     def query_deposit_address(self, **kwargs):
         """
+        :param kwargs: See
+            https://bybit-exchange.github.io/docs/account_asset/#t-deposit_addr_info.
+        :returns: Request results as dictionary.
+
+        """
+        return self._submit_request(
+            method="POST",
+            path=self.endpoint + "/asset/v1/private/transferable-subs/save",
+            query=kwargs,
+            auth=True
+        )
+
+    def enable_universal_transfer(self, **kwargs):
+        """
+        :param kwargs: See
+            https://bybit-exchange.github.io/docs/account_asset/#t-deposit_addr_info.
+        :returns: Request results as dictionary.
+
+        """
+        return self._submit_request(
+            method="POST",
+            path=self.endpoint + "/asset/v1/private/transferable-subs/save",
+            query=kwargs,
+            auth=True
+        )
+
+    def create_universal_transfer(self, **kwargs):
+        """
+        :param kwargs: See
+            https://bybit-exchange.github.io/docs/account_asset/#t-deposit_addr_info.
+        :returns: Request results as dictionary.
+
+        """
+        return self._submit_request(
+            method="POST",
+            path=self.endpoint + "/asset/v1/private/universal/transfer",
+            query=kwargs,
+            auth=True
+        )
+
+    def query_universal_transfer_list(self, **kwargs):
+        """
         Rules: Subaccount can not deposit
-         
+
         :param kwargs: See
             https://bybit-exchange.github.io/docs/account_asset/#t-deposit_addr_info.
         :returns: Request results as dictionary.
@@ -209,7 +251,7 @@ class HTTP(_HTTPManager):
         """
         return self._submit_request(
             method="GET",
-            path=self.endpoint + "/asset/v1/private/deposit/address",
+            path=self.endpoint + "/asset/v1/private/universal/transfer/list",
             query=kwargs,
             auth=True
         )
