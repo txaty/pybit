9961741637f04a637bb3f203b2caf1f416f5b360
txaty <xtianae@connect.ust.hk>
2023-03-15 19:09:33 +0800

    Add more HTTP v5 APIs.

--- pybit/market_v5.py
+++ pybit/market_v5.py
@@ -36,3 +36,76 @@ class HTTP(_V5HTTPManager):
             query=kwargs,
             auth=True,
         )
+        
+    def query_instrument_info(self, **kwargs):
+        return self._submit_request(
+            method="GET",
+            path=self.endpoint + "/v5/market/instruments-info",
+            query=kwargs,
+            auth=True,
+        )
+        
+    def query_orderbook(self, **kwargs):
+        return self._submit_request(
+            method="GET",
+            path=self.endpoint + "/v5/market/orderbook",
+            query=kwargs,
+            auth=True,
+        )
+        
+    def query_tickers(self, **kwargs):
+        return self._submit_request(
+            method="GET",
+            path=self.endpoint + "/v5/market/tickers",
+            query=kwargs,
+            auth=True,
+        )
+
+    def query_funding_rate_history(self, **kwargs):
+        return self._submit_request(
+            method="GET",
+            path=self.endpoint + "/v5/market/funding/history",
+            query=kwargs,
+            auth=True,
+        )
+        
+    def query_public_trading_history(self, **kwargs):
+        return self._submit_request(
+            method="GET",
+            path=self.endpoint + "/v5/market/recent-trade",
+            query=kwargs,
+            auth=True,
+        )
+        
+    def query_open_interest(self, **kwargs):
+        return self._submit_request(
+            method="GET",
+            path=self.endpoint + "/v5/market/open-interest",
+            query=kwargs,
+            auth=True,
+        )
+        
+    def query_historical_volatility(self, **kwargs):
+        return self._submit_request(
+            method="GET",
+            path=self.endpoint + "/v5/market/historical-volatility",
+            query=kwargs,
+            auth=True,
+        )
+        
+    def query_risk_limit(self, **kwargs):
+        return self._submit_request(
+            method="GET",
+            path=self.endpoint + "/v5/market/risk-limit",
+            query=kwargs,
+            auth=True,
+        )
+        
+    def query_option_delivery_price(self, **kwargs):
+        return self._submit_request(
+            method="GET",
+            path=self.endpoint + "/v5/market/delivery-price",
+            query=kwargs,
+            auth=True,
+        )
+        
